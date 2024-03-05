b8822d392c268e78bb1cf10e52d7a450329c0c85
ElsaneHesenova <160469303+ElsaneHesenova@users.noreply.github.com>
2024-03-05 09:52:32 -0800

    Add files via upload

---
+++ ev tapsirigi/script.js
@@ -0,0 +1,64 @@
+  
+
+// let age = +prompt("necenci sinfe gedilmelidir");
+// if (age > 6 && age < 10) {
+//     console.log("Siz ibtidai sinife gede bilersiniz")
+// } else if (age >= 10 && age <= 14) {
+//     console.log("Siz orta mektebe gedirsiniz");
+// } else if (age >= 15 && age <= 17) {
+//     console.log("Siz tam orta mektebe gedirsiniz");
+// }
+ 
+// else if (age >=17 && age < 21) {
+//     console.log("Siz universitetde oxuyursunuz");
+// }
+
+
+// else if (age >= 21) {
+//     console.log("is heyatindasiniz");
+// }
+
+
+// else{
+//     console.log(" siz azyaslisiniz")
+// }
+
+
+//      TASK2
+// let age = +prompt('yasinizi daxil edin');
+// if( age>=18 && age<=70){
+// console.log("ses vermede istirak ede bilersiniz")
+// }
+
+// else if(age>70){
+// console.log(`ses vermeye ${age-70} il gecikmisiniz`);
+// }
+
+// else if(age<18){
+//     console.log(`ses vermesi ucun ${age}il artiq zamani var`)
+
+// }
+
+// else{
+//     console.log("istirakiniz qadagandir")
+// }
+
+
+
+// let num = +prompt("ededi daxil edin");
+// if(num>0){
+//     console.log("eded musbetdir");
+
+// }
+
+// else if(num<0){
+//     console.log("eded menfidir");
+// }
+
+\
+
+let num=[1,3,5,7,];
+let num=[2,4,6,8]
+for(let i=0; i==)
+
+
